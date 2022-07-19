fc2d11062c987a93da8925580259beed2dfef2f7
Gaurav Patel <gaurav.gkp29@gmail.com>
2022-07-19 15:08:14 +0530

    changes

--- model_handlers/user-handler.js
+++ model_handlers/user-handler.js
@@ -408,7 +408,7 @@ const meetupNotification = async(requestParam, req) => {
                 reject(errors(labels.LBL_USER_NOT_FOUND[config.default_language], responseCodes.ResourceNotFound));
                 return;
             }
-            let lists = await query.selectWithAndFilter(dbConstants.dbSchema.meetups, {friend_ids:{$in:[requestParam.user_id]}}, { _id:0, meetup_id: 1, title:1, description:1, friend_ids:1, date:1, time:1, duration:1}, {
+            let lists = await query.selectWithAndFilter(dbConstants.dbSchema.meetups, {friend_ids:{$in:[requestParam.user_id]}}, { _id:0, meetup_id: 1, title:1, description:1, friend_ids:1, date:1, time:1, duration:1, accepted:1, rejected:1}, {
                 created_at: -1,
             }, {
                 skip,
